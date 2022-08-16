5470d87c27d9f7d6f6288fca96743411d5b7b672
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2022-08-16 09:53:47 +0200

    maj

---
+++ rea1/programmation/script.js
@@ -0,0 +1,12 @@
+var slide = new Array("../images/imgCarusel01.jpg", "../images/imgYvon.jpg")
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+setInterval("ChangeSlide(1)", 3000);
