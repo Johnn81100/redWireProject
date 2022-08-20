16e61fba7b68acdaa9ef862357aff70dd74f812b
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2022-08-20 12:16:51 +0200

    MAJ

--- rea1/programmation/script.js
+++ rea1/programmation/script.js
@@ -1,12 +1,26 @@
-var slide = new Array("../images/imgCarusel01.jpg", "../images/imgYvon.jpg")
-var numero = 0;
+let  slide = new Array("../images/imgCarusel01.jpg", "../images/imgYvon.jpg")
+let numero = 0;
 
-function ChangeSlide(sens) {
-    numero = numero + sens;
+function ChangeSlide(sens)
+{
+       numero = numero + sens;
+    
     if (numero < 0)
-        numero = slide.length - 1;
+    {
+        numero = slide.length - 1; 
+    }
     if (numero > slide.length - 1)
+    {
         numero = 0;
-    document.getElementById("slide").src = slide[numero];
+    }
+    if(numero == 1)
+    {
+        document.getElementById("slide").style.objectPosition = "32% 32%";
+    }
+    else
+    {
+        document.getElementById("slide").style.objectPosition = "50% 50%";
+    }
+    document.getElementById("slide").src = slide[numero];   
 }
-setInterval("ChangeSlide(1)", 3000);
+setInterval("ChangeSlide(1)", 3000); /* appui toutes les trois secondes pour changer d'images*/
