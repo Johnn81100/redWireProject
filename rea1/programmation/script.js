9d2b325eefa23e9b95500316c14cb26152621631
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2022-08-30 03:05:13 +0200

    upgrade programmation

--- rea1/programmation/script.js
+++ rea1/programmation/script.js
@@ -5,11 +5,11 @@ function ChangeSlide(sens)
 {
        numero = numero + sens;
     
-    if (numero < 0)
+    if (numero < 0) // si l index de la  variable est plus petit  que le nombre d  images alors  on revient a la derniére image .
     {
         numero = slide.length - 1; 
     }
-    if (numero > slide.length - 1)
+    if (numero > slide.length - 1) // si l index de la  variable est plus grand  que le nombre d  images alors  on revient a la prmeiere image .
     {
         numero = 0;
     }
@@ -24,3 +24,22 @@ function ChangeSlide(sens)
     document.getElementById("slide").src = slide[numero];   
 }
 setInterval("ChangeSlide(1)", 3000); /* appui toutes les trois secondes pour changer d'images*/
+
+
+
+
+//***************************************************menu burger */
+
+
+const navBar = document.querySelector('#navBar');
+const burger = document.querySelector('.burger');
+
+function toogleMenu()
+{
+    burger.addEventListener('click', ()=>
+    {
+        navBar.classList.toggle('openNavBar');
+    })
+}
+
+toogleMenu();
